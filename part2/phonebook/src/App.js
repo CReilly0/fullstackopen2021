b6f54a095389ed9ae00fb464ff785c5bdf59f8ac
CReilly0 <cormacreilly0@gmail.com>
2022-01-25 22:10:28 +0000

    2.9*: The Phonebook Step4
    Implement a search field that can be used to filter the list of people by name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
+import Filter from './Components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (e) => { setNewName(e.target.value) }
+  const handleNumberChange = (e) => { setNewNumber(e.target.value) }
+  const handleFilterChange = (e) => { setFilter(e.target.value) }
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  //If no filter, return persons. If filter, return filtered persons
+  const personsToShow = (filter==="")
+  ? persons
+  : persons.filter((person) => 
+  (person.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1))
 
   const addName = (event) => {
     event.preventDefault()
@@ -30,19 +32,22 @@ const App = () => {
       setPersons(persons.concat(personObject))
     )
     setNewName("")
+    setNewNumber("")
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h3>Phonebook</h3>
+      <Filter handleFilterChange={handleFilterChange} />
+      <h3>Add a new</h3>
       <PersonForm 
         addName={addName} 
         newName={newName} 
         handleNameChange={handleNameChange} 
         newNumber={newNumber} 
         handleNumberChange={handleNumberChange}/>
-      <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
     </div>
   )
 }
--- part2/phonebook/src/Components/Persons.js
+++ part2/phonebook/src/Components/Persons.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, filter }) => {
+
   return(
-    <table>
-      <tbody>
+    <>
+      <table>
+        <tbody>
         {persons.map((person) => 
           <tr key={person.name}>
             <td>{person.name}</td>
             <td>{person.number}</td>
           </tr>
         )}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </>
   )
 };
 
