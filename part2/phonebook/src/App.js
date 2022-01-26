8424427484d6b0af7bdafa1cded01305ba669781
CReilly0 <cormacreilly0@gmail.com>
2022-01-26 18:02:54 +0000

    2.15: Phonebook step7
    Currently, the numbers that are added to the phonebook are not saved to a backend server. Fix this situation.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -38,7 +38,11 @@ const App = () => {
       window.alert(`${newName} is already added to phonebook`);
     }
     else (
-      setPersons(persons.concat(personObject))
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(personObject))
+      })
     )
     setNewName("")
     setNewNumber("")
