d929fb7bf034ddd17812a7868d696ee79de1dd71
CReilly0 <cormacreilly0@gmail.com>
2022-01-26 22:51:31 +0000

    2.19: Phonebook step11
    Use the improved error message example from part 2 as a guide to show a notification that lasts for a few seconds after a successful operation is executed (a person is added or a number is changed):

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,7 @@ import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
 import Filter from './Components/Filter'
 import phonebookService from './Services/phonebook'
+import Notification from './Components/Notification'
 
 const App = () => {
 
@@ -10,11 +11,12 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState(null)
 
   const handleNameChange = (e) => { setNewName(e.target.value) }
   const handleNumberChange = (e) => { setNewNumber(e.target.value) }
   const handleFilterChange = (e) => { setFilter(e.target.value) }
-  
+
   const handleDelete = ({ person }) => {
     const newPersons = persons.filter((p) => p.id !== person.id);
     const confirmDelete = window.confirm(`Delete ${person.name}?`)
@@ -55,19 +57,26 @@ const App = () => {
         phonebookService
         .update(oldPerson.id, newPerson)
         .then(resultPerson => {
-          setPersons(persons.map(p => p.id !== oldPerson.id ? p : resultPerson))
+          setPersons(persons.map(p => p.id !== oldPerson.id ? p : resultPerson));
+          setNotification(`${resultPerson.name}'s number has been changed.`);
+          setTimeout(() => {
+          setNotification(null)
+          }, 5000);
         }
         )
       }
     }
     //else add new person
-    else (
+    else {
       phonebookService
       .create(personObject)
       .then(response => {
-        setPersons(persons.concat(personObject))
-      })
-    )
+        setPersons(persons.concat(personObject));
+        setNotification(`Added ${personObject.name}`);
+        setTimeout(() => {
+          setNotification(null)
+      }, 5000);
+    })}
     //reset forms 
     setNewName("")
     setNewNumber("")
@@ -76,6 +85,7 @@ const App = () => {
   return (
     <div>
       <h3>Phonebook</h3>
+      <Notification message={notification} />
       <Filter handleFilterChange={handleFilterChange} />
       <h3>Add a new</h3>
       <PersonForm 
